3ad09543699fef4f4e6078f73d9ac52533f535f4
n1ckfg <nick@fox-gieg.com>
2018-07-30 13:24:08 -0400

    fix build script

--- Source/LineRenderer/LineRenderer.Build.cs
+++ Source/LineRenderer/LineRenderer.Build.cs
@@ -1,31 +1,18 @@
-// Copyright 2016, Sigurdur Gunnarsson. All Rights Reserved. 
-
 using UnrealBuildTool;
 
 public class LineRenderer : ModuleRules
 {
-	public LineRenderer(TargetInfo Target)
+	public LineRenderer(ReadOnlyTargetRules Target) : base(Target)
 	{
 		
 		PublicIncludePaths.AddRange(new string[] { "LineRenderer/Public" });
 
         PrivateIncludePaths.AddRange(new string[] { "LineRenderer/Private" });
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "RenderCore", "ShaderCore", "RHI", "RuntimeMeshComponent" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core" });
 
+        PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Slate", "SlateCore" });
 
-        PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-// 				"Slate",
-// 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(new string[] {  });
+        DynamicallyLoadedModuleNames.AddRange(new string[] { });
     }
 }
